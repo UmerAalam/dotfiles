5c4aeef0d8c972fd95dceaf4041b0cb341ca7c67
UmerAalam <umerrazzaq.dev@gmail.com>
2025-10-17 12:10:06 +0500

    new deno config

---
+++ .config/fish/conf.d/deno.fish
@@ -0,0 +1,1 @@
+source "/home/umeraalam/.deno/env.fish"
--- .config/nvim/lua/plugins/css.lua
+++ .config/nvim/lua/plugins/css.lua
@@ -2,7 +2,7 @@
 return {
   -- Make Mason install the CSS language server package
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
